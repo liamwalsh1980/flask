dcb311d9a5e777e9c5b4e80d0f82256916916bf8
Liam Walsh <lwalsh_1980@hotmail.co.uk>
2021-07-24 20:54:24 +0000

    Providing Feedback To The User on contact form page

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 import json
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -34,10 +37,8 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form['name'])
-        print(request.form.get("email"))
-        print(request.form.get("phone"))
-        print(request.form.get("message"))
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
